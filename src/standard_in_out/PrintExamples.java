b24a01db081a0f2da41c1dba2cf293db3a636dfd
Piotr Andrzejak <pandrzejak93@gmail.com>
2022-10-11 20:27:53 +0200

    print examples

--- src/standard_in_out/PrintExamples.java
+++ src/standard_in_out/PrintExamples.java
@@ -1,2 +1,16 @@
-package standard_in_out;public class PrintExamples {
+package standard_in_out;
+
+public class PrintExamples {
+
+    public static void main(String[] args) {
+        int myNumber = 125;
+        System.out.println("This is a simple text");
+        System.out.println(myNumber);
+
+        double result = 100.0 / 3.0;
+        System.out.printf("100.0 / 3.0 = %5.2f \n", 100.0 / 3.0);
+        System.out.printf("100.0 / 3.0 = %5.2f \n", result);
+
+        System.out.printf("100 / 3 = %4d", 100 / 3);
+    }
 }
