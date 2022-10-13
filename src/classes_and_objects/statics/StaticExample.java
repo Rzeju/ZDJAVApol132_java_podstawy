fa79b40a8bccf1c2787951a083d5960b73e9af6a
Piotr Andrzejak <pandrzejak93@gmail.com>
2022-10-13 20:43:58 +0200

    static classes

---
+++ src/classes_and_objects/statics/StaticExample.java
@@ -0,0 +1,33 @@
+package classes_and_objects.statics;
+
+public class StaticExample {
+
+    //Deklaracja i inicjalizacja zmiennej statycznej - licznik
+    //(zmienna klasowa - współna dla wszystkich obiektów)
+    //(Metoda statyczna nie potrzebuje obiektu aby ją wywołać)
+    private static int counter = 0;
+
+    //Konstruktor
+    //(zwiększa wartość licznika po każdym utworzonym obiekcie)
+    public StaticExample() {
+        counter++;
+    }
+
+    //Metoda statyczna (klasowa)
+    //Zwraca wartość licznika (counter)
+    public static int getCounter() {
+        return counter;
+    }
+
+    //Metoda zwracająca wartośc licznika (counter)
+    //(Nie jest statyczna, potrzeba obiektu aby ją wywołać
+    public int getCounterByObject() {
+        return counter;
+    }
+
+    //Metoda statyczna ustawiająca wartość licznika (counter)
+    //(Metoda statyczna nie potrzebuje obiektu aby ją wywołać)
+    public static void setCounter(int newCounter) {
+        counter = newCounter;
+    }
+}
