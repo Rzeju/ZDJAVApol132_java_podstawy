00ba58e9234df807e5ec7071b04448da6b07758c
Piotr Andrzejak <pandrzejak93@gmail.com>
2022-10-25 19:43:00 +0200

    Instant example

---
+++ src/data_time/InstantExample.java
@@ -0,0 +1,19 @@
+package data_time;
+
+import java.time.Instant;
+
+public class InstantExample {
+
+    public static void main(String[] args) {
+
+        //Utworzenie obiektu klasy Instant metodą statyczną 'now()'
+        Instant now = Instant.now();
+        //Wyświetlenie liczby milisekund
+        System.out.println(now.toEpochMilli());
+        //Wyświetlenie liczby nanosekund (bez uwzględnienia milisekund)
+        System.out.println(now.getNano());
+        //Wyśiwetlenie obiektu now ( uruchamia się jego metoda 'toString()' )
+        System.out.println(now);
+
+    }
+}
