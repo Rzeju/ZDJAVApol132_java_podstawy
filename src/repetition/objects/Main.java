8a6d818e7f7fb9a12a1f90aaad1dd84b7bb190f4
Piotr Andrzejak <pandrzejak93@gmail.com>
2022-10-26 20:23:28 +0200

    objects repetition

---
+++ src/repetition/objects/Main.java
@@ -0,0 +1,15 @@
+package repetition.objects;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        User user_1 = new User();
+
+        System.out.println(user_1);
+
+        User user_2 = new User("PA", "123", "pa@gmail.com");
+
+        System.out.println(user_2);
+    }
+}
---
+++ src/repetition/objects/User.java
@@ -0,0 +1,20 @@
+package repetition.objects;
+
+public class User {
+
+    private String login;
+    private String password;
+    private String emailAddress;
+
+    public User() {
+
+    }
+
+    public User(String login, String password, String emailAddress) {
+        this.login = login;
+        this.password = password;
+        this.emailAddress = emailAddress;
+    }
+
+
+}
