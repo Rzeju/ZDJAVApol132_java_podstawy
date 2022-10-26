9f9f0d7bcfb48380e9c58bc9078dee44813cd8da
Piotr Andrzejak <pandrzejak93@gmail.com>
2022-10-26 20:28:51 +0200

    objects repetition

--- src/repetition/objects/User.java
+++ src/repetition/objects/User.java
@@ -1,15 +1,20 @@
 package repetition.objects;
 
+//moja klasa
 public class User {
 
+    //pola klasy (każdy obiekt tej klasy zawiera swój własny zestaw tych pól)
     private String login;
     private String password;
     private String emailAddress;
 
+    //konstruktor bezargumentowy (domyślny)
+    //Tworzy obiekt klasy bez przypisywania polom obiektu konkretnych wartości (wartości pól będą domyślne)
     public User() {
 
     }
 
+    //Konstruktor parametrowy, przyjmuje wartości i przypisuje je do pól obiektu (inicjalizuje pola obiektu konkretnymi wartościami)
     public User(String login, String password, String emailAddress) {
         this.login = login;
         this.password = password;
